0ae8b7ef750ca0824103b7b5edab67d1c33ce073
Zhihang Shao <zhihang.shao.iscas@gmail.com>
2024-12-12 01:16:40 +0800

    re: Week1 finished some syscalls

--- src/syscall_imp/time.rs
+++ src/syscall_imp/time.rs
@@ -1,5 +1,11 @@
-use arceos_posix_api as api;
+use core::ffi::c_int;
+
+use arceos_posix_api::{self as api, ctypes::timeval};
 
 pub(crate) fn sys_clock_gettime(clock_id: i32, tp: *mut api::ctypes::timespec) -> i32 {
     unsafe { api::sys_clock_gettime(clock_id, tp) }
+}
+
+pub(crate) fn sys_get_time_of_day(ts: *mut timeval) -> c_int {
+    unsafe { api::sys_get_time_of_day(ts) }
 }
